f15109515630b1ff75c626de7747b88b548717c6
vek422 <vedantkotkar111@gmail.com>
2023-04-04 03:36:13 +0530

    minor changes

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ dotenv.config();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
